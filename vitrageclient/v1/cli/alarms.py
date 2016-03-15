205fab512d7756739973631c400fd9614c1a75fc
Alexey Weyl <alexey.weyl@nokia.com>
2016-03-15 16:12:37 +0200

    change severity to normalized_severity
    Change-Id: I9ece44b22c42b8d980c626cf632222fe0be8a4e2

--- vitrageclient/v1/cli/alarms.py
+++ vitrageclient/v1/cli/alarms.py
@@ -32,5 +32,5 @@ class AlarmsList(lister.Lister):
         vitrage_id = parsed_args.vitrage_id
         alarms = self.app.client.alarms.list(vitrage_id=vitrage_id)
         return utils.list2cols(('type', 'name', 'resource_type',
-                                'resource_id', 'severity',
+                                'resource_id', 'normalized_severity',
                                 'update_timestamp'), alarms)
