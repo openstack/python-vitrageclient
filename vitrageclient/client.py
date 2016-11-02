664905565f7efea624630664e7ac56aecd4d3246
Luong Anh Tuan <tuanla@vn.fujitsu.com>
2016-11-02 16:24:56 +0700

    Make method import_versioned_module work
    Update function import_versioned_module in Oslo.utils 3.17
    This patch update to meet new version. For more information:
    http://docs.openstack.org/developer/oslo.utils/history.html
    
    Change-Id: I9c3ba7acc9b04e766ae4e55f817a5c876a0e8752
    Closes-Bug: #1627313

--- vitrageclient/client.py
+++ vitrageclient/client.py
@@ -13,12 +13,13 @@
 import exc
 
 from keystoneauth1 import adapter as keystoneauth
-from vitrageclient.common import utils
+from oslo_utils import importutils
 
 
 # noinspection PyPep8Naming
 def Client(version, *args, **kwargs):
-    module = utils.import_versioned_module(version, 'client')
+    module = importutils.import_versioned_module('vitrageclient',
+                                                 version, 'client')
     client_class = getattr(module, 'Client')
     return client_class(*args, **kwargs)
 
