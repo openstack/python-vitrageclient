ed9c28867bf433628ba0a57779159a9bbb72aa50
Idan Hefetz <idan.hefetz@nokia.com>
2018-01-02 12:32:15 +0000

    add alarm show to client
    Depends-On: Ia675c6d74ec947729d4520007eac1423cdf58ce9
    Change-Id: I4be7ebc7e977ae8ea0a2daf86777ba08f1d4ad61
    Implements: blueprint add-alarm-show-api

--- vitrageclient/v1/cli/alarm.py
+++ vitrageclient/v1/cli/alarm.py
@@ -52,6 +52,20 @@ class AlarmList(lister.Lister):
                                 'update_timestamp'), alarms)
 
 
+class AlarmShow(show.ShowOne):
+    """Show an alarm"""
+
+    def get_parser(self, prog_name):
+        parser = super(AlarmShow, self).get_parser(prog_name)
+        parser.add_argument("vitrage_id", help="Vitrage id of the alarm")
+        return parser
+
+    def take_action(self, parsed_args):
+        vitrage_id = parsed_args.vitrage_id
+        alarm = utils.get_client(self).alarm.get(vitrage_id=vitrage_id)
+        return self.dict2columns(alarm)
+
+
 # noinspection PyAbstractClass
 class AlarmCount(show.ShowOne):
     """Show a count of all alarms"""
