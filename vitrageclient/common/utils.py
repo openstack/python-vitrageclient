33720fc14ccfc1d1d417dcba6fbcf36f0d563096
Ifat Afek <ifat.afek@nokia.com>
2018-07-24 07:27:09 +0000

    Bugfix: support vitrage alarm list for alarm with no resource
    Change-Id: I5d0693db2c5bad67bf72606aec90425ea05cd9f1
    Story: 2003065
    Task: 23107

--- vitrageclient/common/utils.py
+++ vitrageclient/common/utils.py
@@ -27,7 +27,7 @@ def list2cols(cols, objs):
 def list2cols_with_rename(names_and_keys, objs):
     cols = [i[0] for i in names_and_keys]
     keys = [i[1] for i in names_and_keys]
-    return cols, [tuple([o[k] for k in keys])
+    return cols, [tuple([o.get(k, '') for k in keys])
                   for o in objs]
 
 
